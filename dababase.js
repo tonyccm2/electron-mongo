8636d67e6d06bdc213cfd5eaeb3a5b7cd442db35
yandert <yandert@gmail.com>
2021-08-11 12:10:45 -0500

    Borrando archivos

--- dababase.js
+++
@@ -1,8 +0,0 @@
-const  mongoose =require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/alumnodb',{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.log(err));
--- index.js
+++
@@ -1,7 +0,0 @@
-const {createWindow} = require('./main')
-const {app} = require('electron')
-require('./dababase')
-
-
-app.whenReady().then(createWindow)
-
